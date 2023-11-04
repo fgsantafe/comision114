6987cf34ad11e8a9fc0baefeb1149092af816059
fgsantafe <148561246+fgsantafe@users.noreply.github.com>
2023-11-04 15:40:11 -0300

    Delete src/main/java/edu/ap4t2 directory

--- src/main/java/edu/ap4t2/model/Operacion.java
+++
@@ -1,8 +0,0 @@
-package edu.ap4t2.model;
-
-@FunctionalInterface
-public interface Operacion {
-
-  public String procesar(String s);
-
-}
--- src/main/java/edu/ap4t2/model/SubMateria.java
+++
@@ -1,9 +0,0 @@
-package edu.ap4t2.model;
-
-public class SubMateria extends Materia {
-
-  public SubMateria(String nombre) {
-    super(nombre);
-  }
-
-}
